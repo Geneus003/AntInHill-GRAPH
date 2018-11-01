a41b574084e0c0cef62ddcf4027468604727eefc
Михаил Гумеров <32883438+ymnukvbif@users.noreply.github.com>
2018-11-02 01:29:33 +0300

    Update coeff.py

--- podbor_coeff/coeff.py
+++ podbor_coeff/coeff.py
@@ -1,25 +1,36 @@
 import MinRoadAlg
 import figny
 
-le_road, graph, n = MinRoadAlg.StartProc()
-le_road1, graph1, n1 = MinRoadAlg.StartProc()
-test = [10000, 0, 0, 0, 0]
+number_graph = 10
+number_test = 10000
+
+
+le_road, graph, n = [], [], []
+for i in range(number_graph):
+	le_road += [0]
+	graph += [0]
+	n += [0]
+	le_road[i], graph[i], n[i] = MinRoadAlg.StartProc()
+le_road_sum = sum(le_road)
+
+Good = [10000, 0, 0, 0, 0]
 j = 0
 
-for a in range(2, 40, 2):
-	for b in range(2, 40, 2):
-		for k in range(5, 100, 5):
-			for e in range(5, 100, 5):
+for a in range(1, 45, 1):
+	for b in range(1, 45, 1):
+		for k in range(1, 100, 1):
+			for e in range(1, 100, 1):
 				L = 0
 				j +=1
-        
+				
 				print("Time:", j, [a/10, b/10, k/10, e/10])
-        
-				for i in range(100):
-					L += figny.formicaio_main(graph, n, [a/10, b/10, k/10, e/10]) + figny.formicaio_main(graph1, n1, [a/10, b/10, k/10, e/10])
-          
-				if abs(L/100 - (le_road + le_road1)) < test[0]:
-					test = [abs(L/100 - (le_road + le_road1)), a/10, b/10, k/10, e/10]
-					print("Good:", test)
-          
+				
+				for i in range(number_test):
+					for l in range(number_graph):
+						L += figny.formicaio_main(graph[l], n[l], [a/10, b/10, k/10, e/10])
+					
+				if abs(L/number_test - le_road_sum) < Good[0]:
+					Good = [abs(L/number_test - le_road_sum), a/10, b/10, k/10, e/10]
+					print("Good:", Good)
+				
 print(test)
