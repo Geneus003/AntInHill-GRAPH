95f1e5ec16497033530c49f5598354636e80a2b5
Михаил Гумеров <32883438+ymnukvbif@users.noreply.github.com>
2018-11-06 20:31:15 +0300

    Update coeff.py

--- podbor_coeff/coeff.py
+++ podbor_coeff/coeff.py
@@ -1,33 +1,35 @@
-import MinRoadAlg
+import perebor
 import figny
+import graph
 
-number_graph = 10
+number_matrix = 10
 number_test = 10000
 
 
-le_road, graph, n = [], [], []
-for i in range(number_graph):
+le_road, matrix, n = [], [], []
+for i in range(number_matrix):
 	le_road += [0]
-	graph += [0]
+	matrix += [0]
 	n += [0]
-	le_road[i], graph[i], n[i] = MinRoadAlg.StartProc()
+	matrix[i], n[i] = graph.generate_graph()
+	le_road[i] = perebor.mainus(matrix[i], n[i])
 le_road_sum = sum(le_road)
 
-Good = [10000, 0, 0, 0, 0]
+Good = [100000000, 0, 0, 0, 0]
 j = 0
 
-for a in range(1, 45, 1):
-	for b in range(1, 45, 1):
-		for k in range(1, 100, 1):
-			for e in range(1, 100, 1):
+for a in range(1, 45, 4):
+	for b in range(1, 45, 4):
+		for k in range(1, 100, 5):
+			for e in range(1, 100, 5):
 				L = 0
 				j +=1
 				
 				print("Time:", j, [a/10, b/10, k/10, e/10])
 				
 				for i in range(number_test):
-					for l in range(number_graph):
-						L += figny.formicaio_main(graph[l], n[l], [a/10, b/10, k/10, e/10])
+					for l in range(number_matrix):
+						L += figny.formicaio_main(matrix[l], n[l], [a/10, b/10, k/10, e/10])
 					
 				if abs(L/number_test - le_road_sum) < Good[0]:
 					Good = [abs(L/number_test - le_road_sum), a/10, b/10, k/10, e/10]
