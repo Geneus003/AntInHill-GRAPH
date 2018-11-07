ea485919899da7a567f98c9b9e10f0dd9a208803
Михаил Гумеров <32883438+ymnukvbif@users.noreply.github.com>
2018-11-08 00:05:22 +0300

    Update coeff.py

--- podbor_coeff/coeff.py
+++ podbor_coeff/coeff.py
@@ -3,7 +3,7 @@ import figny
 import graph
 
 number_matrix = 10
-number_test = 10000
+number_test = 1000
 
 
 le_road, matrix, n = [], [], []
@@ -13,21 +13,23 @@ for i in range(number_matrix):
 	n += [0]
 	matrix[i], n[i] = graph.generate_graph()
 	le_road[i] = perebor.mainus(matrix[i], n[i])
+	print(i)
 le_road_sum = sum(le_road)
 
 Good = [100000000, 0, 0, 0, 0]
 j = 0
 
-for a in range(1, 45, 4):
-	for b in range(1, 45, 4):
-		for k in range(1, 100, 5):
-			for e in range(1, 100, 5):
+for a in range(30, 41, 1):
+	for b in range(32, 45, 1):
+		for k in range(1, 10, 1):
+			for e in range(5, 20, 1):
 				L = 0
 				j +=1
 				
 				print("Time:", j, [a/10, b/10, k/10, e/10])
 				
 				for i in range(number_test):
+					print(i)
 					for l in range(number_matrix):
 						L += figny.formicaio_main(matrix[l], n[l], [a/10, b/10, k/10, e/10])
 					
