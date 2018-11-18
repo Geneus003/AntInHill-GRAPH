970bf10aa740e3eaf1e45fd3562eb8cf70e5b985
Михаил Гумеров <32883438+ymnukvbif@users.noreply.github.com>
2018-11-18 21:32:03 +0300

    Update coeff.py

--- podbor_coeff/coeff.py
+++ podbor_coeff/coeff.py
@@ -7,7 +7,7 @@ Qu_test = 10000
 
 
 le_road, matrix, n = [], [], []
-for i in range(number_matrix):
+for i in range(Qu_matrix):
 	le_road += [0]
 	matrix += [0]
 	n += [0]
@@ -28,13 +28,13 @@ for a in range(25, 45, 1):
 				
 				print("Time:", j, [a/10, b/10, k/10, e/10])
 				
-				for i in range(number_test):
+				for i in range(Qu_test):
 					print(i)
-					for l in range(number_matrix):
+					for l in range(Qu_matrix):
 						L += figny.formicaio_main(matrix[l], n[l], [a/10, b/10, k/10, e/10])
 					
-				if abs(L/number_test - le_road_sum) < Good[0]:
-					Good = [abs(L/number_test - le_road_sum), a/10, b/10, k/10, e/10]
+				if abs(L/Qu_test - le_road_sum) < Good[0]:
+					Good = [abs(L/nQu_test - le_road_sum), a/10, b/10, k/10, e/10]
 					print("Good:", Good)
 				
 print(test)
