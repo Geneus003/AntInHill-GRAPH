87a9de19e7d11d84fec1492d8d5b4dde8f674cfb
Geneus003 <Geneus003@users.noreply.github.com>
2018-11-20 22:20:10 +0300

    Update generate_graph_with_len.py

--- podbor_coeff/generate_graph_with_len.py
+++ podbor_coeff/generate_graph_with_len.py
@@ -7,15 +7,23 @@ def main_func(Qu):
   rezult = {}
   
   for i in range(Qu):
+    print(i)
     matrix, n = graph.generate_graph()
     time_ = time.time()
     len_, road = perebor.mainus(matrix, n)
     time_ = time.time() - time_
-    rezult[i] = [matrix, n, len_, time_]
+    rezult = [matrix, n, len_, time_]
+    f = open('graph_with_len.txt', 'ta', encoding="utf-8")
+    print(rezult[1], rezult[2], rezult[3], file=f)
+    for j in range(rezult[1]):
+      for l in range(rezult[1]):
+        print(rezult[0][j][l], end=" ", file=f)
+      print(file=f)
+
+    print(file=f)
+    f.close()
   return rezult
 
 
 if __name__ == "__main__":
-  f = open('graph_with_len.txt', 'w')
-  f.write(main_func(500))
-  f.close()
+  main_func(500)
