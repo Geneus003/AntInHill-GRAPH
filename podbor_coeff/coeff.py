d482d611e73701e876817b354b841b579314d95b
Михаил Гумеров <32883438+ymnukvbif@users.noreply.github.com>
2018-11-21 19:29:20 +0300

    Update coeff.py

--- podbor_coeff/coeff.py
+++ podbor_coeff/coeff.py
@@ -1,40 +1,41 @@
-import perebor
 import figny
-import graph
-
-Qu_matrix = 100
-Qu_test = 10000
+import Parsing_f
 
 
-le_road, matrix, n = [], [], []
-for i in range(Qu_matrix):
-	le_road += [0]
-	matrix += [0]
-	n += [0]
-	matrix[i], n[i] = graph.generate_graph()
-	le_road[i], rfrfrfrfrffrfrfrfrfiighyivuguivgukguiyv = perebor.mainus(matrix[i], n[i])
-	print(i)
-le_road_sum = sum(le_road)
+Qu_test = 100
+
+con = 0
+pr = 0
+n, le_road, t, matrix = [], [], [], []
+for i in range(500):
+	n_, le_road_, t_, matrix_ = Parsing_f.parser(con + pr)
+	
+	n += [n_]
+	le_road += [le_road_]
+	t += [t_]
+	matrix += [matrix_]
+	pr += n_ + 2
 
 Good = [88005553535, 0, 0, 0, 0]
 j = 0
 
-for a in range(25, 45, 1):
-	for b in range(25, 45, 1):
-		for k in range(1, 100, 1):
-			for e in range(1, 100, 1):
+for a in range(1, 50, 1):
+	for b in range(1, 50, 1):
+		for k in range(5, 1000, 5):
+			for e in range(5, 1000, 1):
 				L = 0
 				j +=1
 				
 				print("Time:", j, [a/10, b/10, k/10, e/10])
 				
+				
 				for i in range(Qu_test):
 					print(i)
-					for l in range(Qu_matrix):
-						L += figny.formicaio_main(matrix[l], n[l], [a/10, b/10, k/10, e/10])
+					for l in range(500):
+						L += figny.formicaio_main(matrix[l], n[l], [a/10, b/10, k/10, e/10]) - le_road[l]
 					
-				if abs(L/Qu_test - le_road_sum) < Good[0]:
-					Good = [abs(L/nQu_test - le_road_sum), a/10, b/10, k/10, e/10]
+				if abs(L/Qu_test) < Good[0]:
+					Good = [abs(L/Qu_test), a/10, b/10, k/10, e/10]
 					print("Good:", Good)
 				
 print(test)
