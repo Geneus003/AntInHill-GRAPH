00f3a4b63b6ff6d5ef42a47b0446b82c90f5bd03
Михаил Гумеров <32883438+ymnukvbif@users.noreply.github.com>
2018-11-21 18:53:47 +0300

    Update How_to_use_parser.py

--- podbor_coeff/How_to_use_parser.py
+++ podbor_coeff/How_to_use_parser.py
@@ -3,10 +3,10 @@ import Parsing_f
 con = 0
 pr = 0
 for i in range(500):
-	a, b, c, matrix = Parsing_f.parser(con + pr)
+	n, l, t, matrix = Parsing_f.parser(con + pr)
 
 	# body of code THIS CONSTRUCTION IS REQUERED !!! ЭТА КОНСТРУКЦИЯ НУЖНА ПРОСТО ДОПИШИ ТЕЛО
 
-	print(a, b, c, matrix)
+	print(n, l, t, matrix)
 
 	pr += a + 2
